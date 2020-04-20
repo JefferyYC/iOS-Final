9a73a2e2cc2d484dd9fb0dfdebfa8383ab8b78ea
Jeffery Chen <jefferyyc@gmail.com>
2020-04-19 21:46:17 -0700

    Initial Commit

---
+++ GoFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoFood
+//
+//  Created by Jeffery Chen on 4/19/20.
+//  Copyright © 2020 iOS Decal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
